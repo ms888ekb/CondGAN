65c0bec5166248c5277f63772d0f3a3cc41203d2
Mikhail Sokolov <sokolov.usmk@gmail.com>
2022-02-10 17:31:42 -0600

    updates 2/9/22

--- main.py
+++ main.py
@@ -6,17 +6,10 @@ import torch
 import argparse
 
 
-LEARNING_RATE_G = 0.001
-LEARNING_RATE_D = 0.0001
-TOTAL_STEPS = 100000
-DECAY_STEP = int(TOTAL_STEPS * .75)
-
-
 def argument_parser():
     pass
 
-
-args = argument_parser()
+# args = argument_parser()
 
 
 def ce_loss():
@@ -39,46 +32,7 @@ def adjust_lr_D(optimizer, i_iter):
 
 def train():
     pass
-# args = argument_parser()
-#
-# segm_loss = nn.CrossEntropyLoss()
-# adv_loss = nn.BCEWithLogitsLoss()
-#
-# interp = torch.nn.Upsample(size=(int(args.input_size_source[0]), int(args.input_size_source[1])),
-#                            mode='bilinear',
-#                            align_corners=False)
-#
-# learning_rate_g = args.learning_rate_G
-# learning_rate_d = args.learning_rate_D
-# total_steps = args.max_iter
-# decay_step = int(total_steps * 0.75)
-#
-# # Define optimizers:
-# params = list(self.net_A_enc.parameters()) + list(self.net_A_dec.parameters()) + \
-#          list(self.net_B_enc.parameters()) + list(self.net_B_dec.parameters())
-#
-# self.optimizer_g = torch.optim.Adam(params, lr=self.learning_rate_g, betas=(0.5, 0.999))
-# self.optimizer_d = torch.optim.Adam(itertools.chain(self.net_d_A.parameters(), self.net_d_B.parameters()),
-#                                     lr=self.learning_rate_d, betas=(0.5, 0.999))
-#
-#
-# source_data_loader = data.DataLoader(GeoDataLoader(x_data=args.source,
-#                                                    size=(int(args.input_size_source[0]),
-#                                                          int(args.input_size_source[1]))),
-#                                      batch_size=args.batch_size,
-#                                      shuffle=True,
-#                                      num_workers=0,
-#                                      pin_memory=True,
-#                                      drop_last=True)
-#
-# target_data_loader = data.DataLoader(GeoDataLoader(x_data=args.target,
-#                                                    size=(int(args.input_size_target[0]),
-#                                                          int(args.input_size_target[1]))),
-#                                      batch_size=args.batch_size,
-#                                      shuffle=True,
-#                                      num_workers=0,
-#                                      pin_memory=True,
-#                                      drop_last=True)
 
 
-
+if __name__ == "__main__":
+    pass
