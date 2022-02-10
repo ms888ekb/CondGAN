849793fe7982eb3155d4942cde9dd7592f6b002e
Mikhail Sokolov <sokolov.usmk@gmail.com>
2022-02-09 21:23:03 -0600

    updates 2/9/22

---
+++ data/dataloader.py
@@ -0,0 +1,71 @@
+from torch.utils import data
+import os
+import numpy as np
+from glob import glob
+from osgeo import gdal, gdalconst
+
+
+class GeoDataLoader(data.Dataset):
+    def __init__(self, x_data=None, y_data=None, size=None,
+                 mean=None, std=None, shuffle=True):
+        if y_data is None:
+            self.iamsource = False
+        else:
+            self.iamsource = True
+        self.source_data = self.__lookup(x_data)
+        if y_data is not None:
+            self.source_labels = y_data
+        self.src_indexes = []
+        self.resize_size = size
+        self.shuffle = shuffle
+        self.dataset_size = None
+        self.mean = 127.5 if mean is None else mean
+        self.std = 127 if std is None else std
+        self.__on_init()
+
+    def get_sample_batch(self, num=1):
+        sample_indexes = np.random.randint(0, high=len(self), size=num)
+        return self[sample_indexes[0]]
+
+    def get_num_samples(self):
+        return self.dataset_size
+
+    def __on_init(self):
+        source_data_index = np.arange(len(self.source_data))
+        if self.shuffle:
+            np.random.shuffle(source_data_index)
+        self.dataset_size = len(source_data_index)
+        self.src_indexes = source_data_index
+
+    @staticmethod
+    def __lookup(path):
+        mask = os.path.join(path, "*.TIF")
+        f_list = glob(mask)
+        return f_list
+
+    def __len__(self):
+        return self.dataset_size
+
+    def __getitem__(self, index):
+        index = self.src_indexes[index]
+        image_ds = gdal.Open(self.source_data[index], gdalconst.GA_ReadOnly)
+        image_georef = image_ds.GetGeoTransform()
+        assert image_ds is not None, "The input image is None. Check the image path."
+        image = image_ds.ReadAsArray()
+        assert image.shape[0] == 4, "Wrong input image band number"
+        image = np.asarray(image, np.float)
+        image = image.transpose((1, 2, 0))
+        image -= self.mean
+        image /= self.std
+        image = image.transpose((2, 0, 1))
+        image = image[:3, :, :]
+        if self.iamsource:
+            sample_name = os.path.split(self.source_data[index])[1]
+            if not os.path.exists(os.path.join(self.source_labels, sample_name)):
+                print(f'Corresponding label for {sample_name} was not found.')
+            label_ds = gdal.Open(os.path.join(self.source_labels, sample_name), gdalconst.GA_ReadOnly)
+            label = label_ds.ReadAsArray()
+            label = np.asarray(label, np.uint8)
+            return image.copy(), label.copy(), image_georef
+        else:
+            return image.copy(), image_georef
--- main.py
+++ main.py
@@ -1,12 +1,84 @@
 from units.net import CondUDA
+from data.dataloader import GeoDataLoader
+from torch.utils import data
+import torch.nn as nn
+import torch
+import argparse
 
 
+LEARNING_RATE_G = 0.001
+LEARNING_RATE_D = 0.0001
+TOTAL_STEPS = 100000
+DECAY_STEP = int(TOTAL_STEPS * .75)
 
 
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+def argument_parser():
     pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+args = argument_parser()
+
+
+def ce_loss():
+    pass
+
+
+def lr_decay(base_lr, iter, max_iter, decay_step):
+    return base_lr * ((max_iter - float(iter)) / float((max_iter - decay_step)))
+
+
+def adjust_lr_G(optimizer, i_iter):
+    lr = lr_decay(args.learning_rate_g, i_iter, args.total_steps, args.decay_step)
+    optimizer.param_groups[0]['lr'] = lr
+
+
+def adjust_lr_D(optimizer, i_iter):
+    lr = lr_decay(args.learning_rate_d, i_iter, args.total_steps, args.decay_step)
+    optimizer.param_groups[0]['lr'] = lr
+
+
+def train():
+    pass
+# args = argument_parser()
+#
+# segm_loss = nn.CrossEntropyLoss()
+# adv_loss = nn.BCEWithLogitsLoss()
+#
+# interp = torch.nn.Upsample(size=(int(args.input_size_source[0]), int(args.input_size_source[1])),
+#                            mode='bilinear',
+#                            align_corners=False)
+#
+# learning_rate_g = args.learning_rate_G
+# learning_rate_d = args.learning_rate_D
+# total_steps = args.max_iter
+# decay_step = int(total_steps * 0.75)
+#
+# # Define optimizers:
+# params = list(self.net_A_enc.parameters()) + list(self.net_A_dec.parameters()) + \
+#          list(self.net_B_enc.parameters()) + list(self.net_B_dec.parameters())
+#
+# self.optimizer_g = torch.optim.Adam(params, lr=self.learning_rate_g, betas=(0.5, 0.999))
+# self.optimizer_d = torch.optim.Adam(itertools.chain(self.net_d_A.parameters(), self.net_d_B.parameters()),
+#                                     lr=self.learning_rate_d, betas=(0.5, 0.999))
+#
+#
+# source_data_loader = data.DataLoader(GeoDataLoader(x_data=args.source,
+#                                                    size=(int(args.input_size_source[0]),
+#                                                          int(args.input_size_source[1]))),
+#                                      batch_size=args.batch_size,
+#                                      shuffle=True,
+#                                      num_workers=0,
+#                                      pin_memory=True,
+#                                      drop_last=True)
+#
+# target_data_loader = data.DataLoader(GeoDataLoader(x_data=args.target,
+#                                                    size=(int(args.input_size_target[0]),
+#                                                          int(args.input_size_target[1]))),
+#                                      batch_size=args.batch_size,
+#                                      shuffle=True,
+#                                      num_workers=0,
+#                                      pin_memory=True,
+#                                      drop_last=True)
+
+
+
